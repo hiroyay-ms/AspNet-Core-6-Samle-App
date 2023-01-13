c23de9e536d1eb9d223fd1ddc51062206617df7f
hiroyay-ms <hiroyay@microsoft.com>
2023-01-13 21:08:00 +0900

    Update mvc project (Web3)

--- src/Web3/Program.cs
+++ src/Web3/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDaprClient();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
